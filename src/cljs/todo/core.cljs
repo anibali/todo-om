5efc4b226d99f5cb793daa65bd14331c1aaf739c
Aiden Nibali <dismaldenizen@gmail.com>
2015-01-20 22:26:55 +1100

    Added Bootstrap

--- src/cljs/todo/core.cljs
+++ src/cljs/todo/core.cljs
@@ -8,15 +8,17 @@
 (defonce app-state (atom {
   :items [{:id 1, :title "Bake a cake", :done true}
           {:id 2, :title "Cake a clown", :done false}
-          {:id 3, :title "Bake a cake", :done true}]}))
+          {:id 3, :title "Bake a cake", :done false}]}))
 
 (defn list-item-view [item]
   (reify
     om/IRenderState
     (render-state [this {:keys [toggle-done]}]
-            (html [:li
-                   {:style {:text-decoration
+            (html [:a
+                   {:class "list-group-item"
+                    :style {:text-decoration
                             (if (item :done) "line-through" "none")}
+                    :href "#"
                     :onClick (fn [e] (put! toggle-done @item))}
                    (item :title)]))))
 
@@ -24,25 +26,32 @@
   (reify
     om/IInitState
     (init-state [_]
-      {:toggle-done (chan)})
+                {:toggle-done (chan)})
     om/IWillMount
     (will-mount [_]
-      (let [toggle-done (om/get-state owner :toggle-done)]
-        (go (loop []
-          (let [item (<! toggle-done)
-                new-item (update-in item [:done] not)]
-            (om/transact! app :items
-              (fn [items]
-                (vec (replace {item new-item} items))))
-            (recur))))))
+                (let [toggle-done (om/get-state owner :toggle-done)]
+                  (go (loop []
+                        (let [item (<! toggle-done)
+                              new-item (update-in item [:done] not)]
+                          (om/transact! app :items
+                                        (fn [items]
+                                          (vec (replace {item new-item} items))))
+                          (recur))))))
     om/IRenderState
     (render-state [this {:keys [toggle-done]}]
-            (html [:div
-                   [:h3 "My awesome tasks"]
-                   [:ul
-                    (om/build-all list-item-view (app :items)
-                                  {:init-state {:toggle-done toggle-done}
-                                   :key :id})]]))))
+                  (html [:div {:class "well-lg"}
+                         [:h3 "My awesome tasks"]
+                         [:div
+                          [:div
+                           {:class "list-group"}
+                           (om/build-all list-item-view (app :items)
+                                         {:init-state {:toggle-done toggle-done}
+                                          :key :id})
+                           [:input
+                            {:class "list-group-item"
+                             :type "text"
+                             :style {:width "100%"}
+                             :placeholder "Add new item..."}]]]]))))
 
 (defn main []
   (om/root list-view app-state
